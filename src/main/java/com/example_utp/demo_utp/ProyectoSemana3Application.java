c9962fe32e8da11273e5063fbc4530c58725bd0a
Joel-HackMaster <122944694+Joel-HackMaster@users.noreply.github.com>
2024-08-28 19:01:29 -0500

    first commit

---
+++ src/main/java/com/example_utp/demo_utp/ProyectoSemana3Application.java
@@ -0,0 +1,13 @@
+package com.example_utp.demo_utp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSemana3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoSemana3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example_utp/demo_utp/ProyectoSemana3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example_utp.demo_utp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSemana3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
